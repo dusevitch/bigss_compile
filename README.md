0ed1746e91637eccbcbf77ff7746eacc59d93574
David <dusevitch@gmail.com>
2021-10-22 17:35:29 -0400

    install script can install to any location, README include instructions to install other dependencies not yet implemented (incomplete)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+
 # Installation of the BIGSS Robotic System
 
 This script installs the full BIGSS Robotic system and needed dependencies. 
@@ -6,7 +8,7 @@ This script installs the full BIGSS Robotic system and needed dependencies.
 
 ### CURRENT DEPENDENCIES
 
-These are not yet automatically loaded
+These are not yet automatically loaded, so be sure they are installed properly on your system. ( SEE document end for install help)
 
 * FTDI Driver
 
@@ -14,7 +16,7 @@ These are not yet automatically loaded
 
 * (not required here, but will be for future versions):
 
-  *  Slicer 4.11 ??
+  *  Slicer 4.13
   * Qt 15.5.2
   * CMAKE 3.19.8
 
@@ -25,14 +27,21 @@ These are not yet automatically loaded
 To install clone this folder and create a new directory:
 
 ```bash
-mkdir bigss
-cd bigss
+mkdir ~/bigss
 ```
 
 Then include this directory as your path for the BIGSS system (will update this later to any path desired)
 
 ```bash
-sudo ./../bigssRoboticSystemInstall.sh -v melodic -b [path_to_cloned_folder]/bigss_compile/bigss [-d true]
+cd [install folder]
+sudo ./bigssRoboticSystemInstall.sh -v melodic -b [path to desired folder location] [-d true]
+```
+
+An example:
+
+```bash
+cd ~/Downloads/bigssRoboticSystem/install
+sudo ./bigssRoboticSystemInstall.sh -b /home/david/bigss
 ```
 
 This will take some time to compile depending on your computer (~4-5 min on avg).  You will be asked for your appropriate GitLab credentials.
@@ -70,11 +79,53 @@ Please include errors you run into in the "Issues" section so I can update this 
 ### TODOS
 
 * Change Installation of bigssRoboticSystem so `catkin build` doesn't have to be run multiple times
-
 * Working Integration with Slicer
-
 * Checking and automatic installation of FTDI and Maxxon Controllers
-
 * update `cisst_repos.rosinstall` to only include necessary repos (i've eliminated most, but I'm sure there are more)
 
-  
+
+
+-----------------------------------------------------------------------------------------------------
+
+
+
+## INSTALLATION GUIDE FOR ADDITIONAL DEPENDENCIES
+
+Eventually we'll remove a few of these guides as we integrate them into the script.
+
+
+
+### FTDI DRIVER
+
+For the  FTDI library:
+
+* Downloads available here: https://ftdichip.com/drivers/d2xx-drivers/
+* Follow the readme here to install: https://www.ftdichip.com/Drivers/D2XX/Linux/ReadMe.txt
+
+
+
+### MAXON MOTOR DRIVER
+
+Navigate to the BIGSS repo called drivers (https://git.lcsr.jhu.edu/bigss/drivers)
+
+EPOS-Linux-Library-En.zip contains the drivers for EPOS2 communication with Maxon motors (used as of 2021 in BIGGS util). Unzip and run install.sh
+
+```bash
+# cd into folder of choice
+unzip EPOS-Linux_Library-En.zip
+chmod +775 install.sh
+sudo ./install.sh
+```
+
+
+
+### SLICER 4.13
+
+
+
+### Qt 15.5.2
+
+
+
+### CMAKE 3.19.8
+
--- bigssRoboticSystemInstall.sh
+++ bigssRoboticSystemInstall.sh
@@ -41,7 +41,7 @@ while getopts v:b:d: flag
 do
     case "${flag}" in
         v) VERSION=${OPTARG};; # get version
-        b) BASE_FOLDER=${OPTARG=~/bigss};; # get base folder name
+        b) BASE_DIR=${OPTARG=~/bigss};; # get base folder name
         d) INSTALL_DEPENDENCIES=${OPTARG}
     esac
 done
@@ -51,13 +51,16 @@ if [ "$VERSION" = "" ]; then
     VERSION=melodic
 fi
 
-if [ "$BASE_FOLDER" = "" ]; then
-    BASE_FOLDER=~/bigss
+if [ "$BASE_DIR" = "" ]; then
+    BASE_DIR=~/bigss
 fi
 
 if [ "$INSTALL_DEPENDENCIES" = "" ]; then
     INSTALL_DEPENDENCIES=true
 fi
+
+# Current Directory where install files are located
+CURRENT_DIR=$(pwd)
 
 #-------------------------------------- DOWNLOAD --------------------------------------
 if [ "$INSTALL_DEPENDENCIES" = true ]; then
@@ -106,16 +109,16 @@ fi
 # # TODO: Check that qmake is at least QT 5.15.2 installed
 
 # Create a workspace
-mkdir -p $BASE_FOLDER/catkin_ws/src
-cd $BASE_FOLDER/catkin_ws
+mkdir -p $BASE_DIR/catkin_ws/src
+cd $BASE_DIR/catkin_ws
 source /opt/ros/$VERSION/setup.bash 
 catkin init
 catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 # run .rosinstall file
 # TODO: determine which of these git files we absolutely need to run our code and remove others
-cd $BASE_FOLDER/catkin_ws #TODO: make this navigate to correct place for rosinstall files
-vcs import < ../../rosinstall_files/cisst_repos.rosinstall # TODO: change the reliant piece of this
+cd $BASE_DIR/catkin_ws #TODO: make this navigate to correct place for rosinstall files
+vcs import < $CURRENT_DIR/rosinstall_files/cisst_repos.rosinstall # TODO: change the reliant piece of this
 vcs pull src
 
 # # -------------------------------------- BUILD --------------------------------------
@@ -123,45 +126,45 @@ echo "------------------BUILDING STARTED-------------------"
 
 # Build cisst-saw
 echo "Building CISST-SAW..........."
-cd $BASE_FOLDER/catkin_ws/src/cisst-saw/sawConstraintController
+cd $BASE_DIR/catkin_ws/src/cisst-saw/sawConstraintController
 git checkout -b bigss_dev_commit 0e604050d9fb04dce64554a471bed915e7288e87 
-cd $BASE_FOLDER/catkin_ws
+cd $BASE_DIR/catkin_ws
 catkin build --summary
 source devel/setup.bash
 
 # IMPORT OTHER REPOS AND BUILD
-cd $BASE_FOLDER
+cd $BASE_DIR
 git config --global credential.helper 'cache --timeout=1200' # TODO make time shorter here
-vcs import < ../rosinstall_files/bigss_repos.rosinstall --workers 1
+vcs import < $CURRENT_DIR/rosinstall_files/bigss_repos.rosinstall --workers 1
 vcs pull src # Update to install the latest versions
 
 # Build BIGSS Util 
 echo "Building BIGSS Util..........."
-cd $BASE_FOLDER/util
+cd $BASE_DIR/util
 mkdir build
 cd build
-cmake .. -GNinja -Dcisst_DIR=$BASE_FOLDER/catkin_ws/devel/cmake -DBIGSS_BUILD_audio=ON -DBIGSS_BUILD_bigssMath=ON  -DBIGSS_BUILD_exe=ON  -DBIGSS_BUILD_fbgInterrogator=ON -DBIGSS_BUILD_filter=ON -DBIGSS_BUILD_maxonControl=ON -DBIGSS_BUILD_maxonUI=ON -DBIGSS_BUILD_polaris=ON  -DBIGSS_BUILD_qled=ON -DBIGSS_BUILD_sharedMemory=ON -DBIGSS_BUILD_universalRobot=ON 
+cmake .. -GNinja -Dcisst_DIR=$BASE_DIR/catkin_ws/devel/cmake -DBIGSS_BUILD_audio=ON -DBIGSS_BUILD_bigssMath=ON  -DBIGSS_BUILD_exe=ON  -DBIGSS_BUILD_fbgInterrogator=ON -DBIGSS_BUILD_filter=ON -DBIGSS_BUILD_maxonControl=ON -DBIGSS_BUILD_maxonUI=ON -DBIGSS_BUILD_polaris=ON  -DBIGSS_BUILD_qled=ON -DBIGSS_BUILD_sharedMemory=ON -DBIGSS_BUILD_universalRobot=ON 
 ninja && ninja install
 
 # # TODO: ADD THIS FOR THE FUTURE?
 # # Install BIGSS snake  
 # echo "Building BIGSS snake..........."
-# cd $BASE_FOLDER/snake
+# cd $BASE_DIR/snake
 # mkdir build
 # cd build
-# cmake .. -GNinja -DBIGSSUtil_DIR=$BASE_FOLDER/util/build 
+# cmake .. -GNinja -DBIGSSUtil_DIR=$BASE_DIR/util/build 
 # ninja
 
 # TODO: update with latest NLOPT version
 # Install NLOPT 
 echo "Installing NLOPT..........."
-cd $BASE_FOLDER/nlopt
-cmake . -GNinja -DNLopt_DIR=$BASE_FOLDER/nlopt/build && ninja && ninja install
+cd $BASE_DIR/nlopt
+cmake . -GNinja -DNLopt_DIR=$BASE_DIR/nlopt/build && ninja && ninja install
 
 # Build bigssRoboticSystem
 echo "Building bigssRoboticSystem..........."
 # TODO ADD bigssRoboticSystem build functions here
-cd $BASE_FOLDER/catkin_ws/
+cd $BASE_DIR/catkin_ws/
 catkin build
 sleep 5s
 catkin build
